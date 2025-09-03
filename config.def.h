661606269e20d0a8d6423923d746f09b43bf998f
alexphanna <ahanna0608@gmail.com>
2025-09-03 17:30:33 -0400

    Fix keybinds

--- config.def.h
+++ config.def.h
@@ -112,7 +112,7 @@ static const Key keys[] = {
 	// { MODKEY|ShiftMask,             XK_h,      setcfact,       {.f = +0.25} },
 	// { MODKEY|ShiftMask,             XK_l,      setcfact,       {.f = -0.25} },
 	// { MODKEY|ShiftMask,             XK_o,      setcfact,       {.f =  0.00} },
-	// { MODKEY,                       XK_Return, zoom,           {0} },
+	{ MODKEY,                       XK_Return, zoom,           {0} },
 	// { MODKEY|Mod4Mask,              XK_u,      incrgaps,       {.i = +1 } },
 	// { MODKEY|Mod4Mask|ShiftMask,    XK_u,      incrgaps,       {.i = -1 } },
 	// { MODKEY|Mod4Mask,              XK_i,      incrigaps,      {.i = +1 } },
