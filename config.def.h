52114012a4ec703eea68c581b8cd3f48596559cc
alexphanna <ahanna0608@gmail.com>
2025-09-24 21:28:07 -0400

    Update font

--- config.def.h
+++ config.def.h
@@ -4,7 +4,7 @@
 /* appearance */
 static const unsigned int borderpx  = 0;        /* border pixel of windows */
 static const unsigned int snap      = 16;       /* snap pixel */
-static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
+static const int swallowfloating    = 1;        /* 1 means swallow floating windows by default */
 static const unsigned int gappih    = 16;       /* horiz inner gap between windows */
 static const unsigned int gappiv    = 16;       /* vert inner gap between windows */
 static const unsigned int gappoh    = 16;       /* horiz outer gap between windows and screen edge */
@@ -12,7 +12,7 @@ static const unsigned int gappov    = 16;       /* vert outer gap between window
 static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "SF Mono:style=semibold:size=10", "Material Symbols Rounded:size=10" };
+static const char *fonts[]          = { "SF Mono:style=semibold:size=10", "Material Icons Round:size=10" };
 static const char dmenufont[]       = "SF Mono:style=semibold:size=10";
 static const char col_black[]       = "#000000";
 static const char col_white[]       = "#FFFFFF";
